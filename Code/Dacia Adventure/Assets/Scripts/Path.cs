c2d06b0f04151702d5a005c0c6be5da6cfcb3af7
Damian Kreick <damian.kreick@globant.com>
2017-08-02 15:28:07 -0300

    Initial commit

---
+++ Code/Dacia Adventure/Assets/Scripts/Path.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour {
+
+	public GameObject path0;
+	public GameObject path1;
+	public float speed;
+
+	void Start () 
+	{
+		speed = 10;
+	}
+	
+	void Update () 
+	{
+		MovePaths ();	
+	}
+
+	void MovePaths()
+	{
+		path0.transform.Translate(Vector3.down * (speed * Time.deltaTime));
+		path1.transform.Translate(Vector3.down * (speed * Time.deltaTime));
+
+		if (path0.transform.position.y < -16f) 
+		{
+			path0.transform.position = new Vector3 (path0.transform.position.x, 4.8f, path0.transform.position.z);
+		}
+		if (path1.transform.position.y < -16f) 
+		{
+			path1.transform.position = new Vector3 (path0.transform.position.x, 4.8f, path0.transform.position.z);
+		}
+	}
+}
---
+++ Code/Dacia Adventure/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed;
+
+	void Start () 
+	{
+		speed = 10;
+	}
+		
+	void Update () 
+	{
+		MovePlayer();
+		ClampPlayer();
+	}
+
+	void MovePlayer () 
+	{
+		if (Input.GetKey(KeyCode.A)) 
+		{
+			transform.Translate(Vector3.left * (speed * Time.deltaTime));
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.Translate(Vector3.right * (speed *Time.deltaTime));
+		}
+	}
+
+	void ClampPlayer ()
+	{
+		Vector3 clampedPosition = transform.position;
+		clampedPosition.x = Mathf.Clamp(transform.position.x, -2.6f, 2.6f);
+		transform.position = clampedPosition;
+	}
+}
